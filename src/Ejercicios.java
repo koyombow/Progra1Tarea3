8c1a8bdf2abf40f0e5e1f68f5a8d5cd0baeaaf0d
Turupawn <turupawn@userr>
2014-08-03 19:19:38 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,59 @@
+
+public class Ejercicios
+{
+	static int sumar(int x, int y)
+	{
+		return -1;
+	}
+	
+	static int restar(int x, int y)
+	{
+		return -1;
+	}
+	
+	static int multiplicar(int x, int y)
+	{
+		return -1;
+	}
+	
+	static int residuo(int x, int y)
+	{
+		return -1;
+	}
+	
+	static boolean esPar(int x)
+	{
+		return false;
+	}
+	
+	static boolean esMultiploDe3(int x)
+	{
+		return false;
+	}
+	
+	static int getMayor(int x,int y)
+	{
+		return -1;
+	}
+	
+	static boolean esMayorDeEdad(int edad)
+	{
+		return false;
+	}
+	
+	static boolean sonPares(int x, int y, int z)
+	{
+		return false;
+	}
+	
+	static int getMayorDe3(int x, int y, int z)
+	{
+		return -1;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
