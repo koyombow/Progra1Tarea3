9bcab1653ca3901e019c84b5b5efa3a11c76ffa2
koyombow <koyombow@users.noreply.github.com>
2014-08-13 20:46:39 -0600

    Update Ejercicios.java

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,64 +1,110 @@
 
+
 public class Ejercicios
 {
 	//devuelve la suma de x y y
 	static int sumar(int x, int y)
+	
 	{
-		return -1;
+		int resultado = x+y;
+		return resultado;
 	}
 	
 	//devuelve la resta de x y y
 	static int restar(int x, int y)
 	{
-		return -1;
+		int resultado = x-y;
+		return resultado;
 	}
 	//devuelve la multiplicacion de x y y
 	static int multiplicar(int x, int y)
 	{
-		return -1;
+		int resultado = x*y;
+		return resultado;
 	}
 	
 	//devuelve el residuo de x y y
 	static int residuo(int x, int y)
 	{
-		return -1;
+		int resultado = x%y;
+		return resultado;
 	}
 	
 	//devuelve true si x es par, de lo cotrario devuelve false
 	static boolean esPar(int x)
 	{
+		if(x % 2 == 0)
+		{
+			return true;
+		}
 		return false;
 	}
 	
 	//devuelve true si x es multiplo de 3, de lo cotrario devuelve false
 	static boolean esMultiploDe3(int x)
+	
 	{
+		if(x % 3 == 0)
+		{
+			return true;
+		}
+		
 		return false;
 	}
 	
 	//devuelve el numero mayor (x o y)
 	static int getMayor(int x,int y)
 	{
-		return -1;
+		if (x>y)
+		{
+			return x;
+		}else if (y>x)
+		{
+		
+		return y;
+		}
 	}
+
 	
 	//devuelve true si la edad es mayor o igual a 18
 	static boolean esMayorDeEdad(int edad)
 	{
+		if (edad>=18)
+		{
+			return true;
+		}
 		return false;
 	}
 	
 	//devuelve true si x, y y z son pares
 	static boolean sonPares(int x, int y, int z)
 	{
+		if((x % 2 ==0)&&( y % 2 == 0)&& (z % 2 == 0))
+		{
+			return true;
+		}
 		return false;
 	}
 	
 	//devuelve el numero mayor entre x, y y z
 	static int getMayorDe3(int x, int y, int z)
 	{
-		return -1;
-	}
+		if((x>y)&&(x>z))
+		{
+			return x;
+		
+		}else if ((y>x)&&(y>z))
+		{
+			return y;
+		}if((z>x)&&(z>y))
+		{
+			
+		}
+		
+			return z;
+		}
+		
+	
 	
 	public static void main(String[] args)
 	{
